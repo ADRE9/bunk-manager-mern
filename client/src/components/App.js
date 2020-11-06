73e1d44ef8720359890ed403a9ecfbcefd7e5fc7
Adrenine <adrenine13@gmail.com>
2020-11-06 22:32:59 +0530

    Client side initialised

---
+++ client/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return ( 
+    <div>
+      Hello React!!
+    </div>
+   );
+}
+ 
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+//my components
+import App from './components/App';
+import reducers from './reducers'; 
+
+const composeEnhancers=window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers,composeEnhancers(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.querySelector('#root'));
