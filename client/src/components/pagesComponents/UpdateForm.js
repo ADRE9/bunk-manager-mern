4a3c94e632c6099941ae143ac28779417c0bfd64
Artistic18 <hrishitbiswas20@gmail.com>
2021-04-02 02:58:01 +0530

    added redirect on dialog close

--- client/src/components/pagesComponents/UpdateForm.js
+++ client/src/components/pagesComponents/UpdateForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import * as Yup from 'yup';
 import Button from '@material-ui/core/Button';
 import {makeStyles} from '@material-ui/core/styles'
