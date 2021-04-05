ece71e8ee505e4a120833a92da7fc564d07b100d
FireQueen-3010 <dikshapatro807@gmail.com>
2021-04-05 12:10:55 +0530

    Made footer mobile responsive and increased icon size

--- client/src/components/pagesComponents/Footer.js
+++ client/src/components/pagesComponents/Footer.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useEffect } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import {  FormControl, TextField, Button } from '@material-ui/core';
 import {  withStyles, makeStyles } from '@material-ui/core/styles';
@@ -69,18 +70,47 @@ const useStyles = makeStyles(theme => ({   //classes of tages below used
     marginBottom: "1%"
 
   },
+
+  upperField2: {
+    display: "grid",
+    gap: " 1rem",
+    gridTemplate: "grid-template-columns",
+    margin: "0% 5% 5% 5%",
+    textAlign: "center",
+    marginBottom: "2%"
+
+  },
+
   margin: {
     width: "100%",
     borderWidth: "2px",
 
   },
+
+  margin2: {
+    width: "100%",
+    borderWidth: "2px",
+
+  },
+
   input: {
     color: 'white',
     fontSize: "1rem",
   },
+
+  input3: {
+    color: 'white',
+    fontSize: "0.7rem",
+  },
+
   inputLable: {
     color: 'white',
   },
+
+  inputLable2: {
+    color: 'white',
+  },
+
   input2: {
     color: 'white',
     fontSize: '1.5rem'
@@ -104,7 +134,16 @@ export default function Footer() {
   const [emailVal, updateEmail] = useState("");
   const [phoneVal, updatePhone] = useState("");
   const [messageVal, updateMessage] = useState("");
+  const [width, setWidth] = React.useState(window.innerWidth);
 
+  const updateWidthAndHeight = () => {
+    setWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateWidthAndHeight);
+    return () => window.removeEventListener("resize", updateWidthAndHeight);
+  });
 
 
   function handleSend(event) {
@@ -120,19 +159,28 @@ export default function Footer() {
 
 
 
-
   return (
     <React.Fragment>
 
       <AppBar color="secondary" className={classes.test}>
+        {width > 720 && (
+        <div>
         <h1 style={{fontFamily:"Arial, sans-serif",fontSize:"300%",marginBottom:"0%"}}>Get In Touch With Bunk Manager</h1>
         <h2 style={{fontFamily:"cursive",marginTop:"0%"}}>We love feedback.Fill out form below and we'll get back to you as soon as possible.</h2>
+        </div>
+        )}
+        {width <= 720 && (
+        <div>
+        <h1 style={{fontFamily:"Arial, sans-serif",fontSize:"150%",marginBottom:"0%"}}>Get In Touch With Bunk Manager</h1>
+        <h2 style={{fontFamily:"cursive",marginTop:"2%",fontSize:"100%", marginRight:"3%", marginLeft:"3%"}}>We love feedback. Fill out form below and we'll get back to you as soon as possible.</h2>
+        </div>
+        )}
         <FormControl style={{ textAlign: "center", margin: "5%" ,marginBottom:"1%",marginTop:"1%" }}>
+         {width > 720 && (
           <div className={classes.upperField}>
             <div> <MyTextField className={classes.margin}
               InputProps={{ className: classes.input, }}
               InputLabelProps={{ className: classes.inputLable }}
-    
               onChange={(event)=>{updateName(event.target.value)}}
               label="Name"
               Type="text"
@@ -140,6 +188,7 @@ export default function Footer() {
               id="custom-css-outlined-input"
             />
             </div>
+         
             <div> <MyTextField className={classes.margin}
               InputProps={{ className: classes.input, }}
               InputLabelProps={{ className: classes.inputLable }}
@@ -160,8 +209,44 @@ export default function Footer() {
               id="custom-css-outlined-input"
             />
             </div>
-
           </div>
+         )}
+          {width <= 720 && (
+          <div className={classes.upperField2}>
+            <div> <MyTextField className={classes.margin2}
+              InputProps={{ className: classes.input3, }}
+              InputLabelProps={{ className: classes.inputLable2 }}
+              onChange={(event)=>{updateName(event.target.value)}}
+              label="Name"
+              Type="text"
+              variant="outlined"
+              id="custom-css-outlined-input"
+            />
+            </div>
+         
+            <div> <MyTextField className={classes.margin2}
+              InputProps={{ className: classes.input3, }}
+              InputLabelProps={{ className: classes.inputLable2 }}
+              onChange={(event)=>{updateEmail(event.target.value)}}
+              label="Email"
+              type="Email"
+              variant="outlined"
+              id="custom-css-outlined-input"
+            />
+            </div>
+            <div> <MyTextField className={classes.margin2}
+              InputProps={{ className: classes.input3, }}
+              InputLabelProps={{ className: classes.inputLable2 }}
+              onChange={(event)=>{updatePhone(event.target.value)}}
+              label="Phone"
+              type="Number"
+              variant="outlined"
+              id="custom-css-outlined-input"
+            />
+            </div>
+          </div>
+         )}
+
           <div> <MyTextField2
             style={{ width: "90%" }}
             multiline rows={8}
@@ -170,11 +255,12 @@ export default function Footer() {
             onChange={(event)=>{updateMessage(event.target.value)}}
             label="Message"
             variant="filled" />
-
+      
           </div>
           <Button variant="contained" color="primary" onClick={handleSend} style={{ width: "90%", left: "5%", right: "5%", marginTop: "1%" }}>
             Send
            </Button>
+           {width > 720 && (
           <div style={{ margin: "1% 1% 0% 1%" }}>
             <a href="https://github.com/ASHISH-GITHUB2495/GRIP_BasicBankingSystem_2021" target="_blank">
               < GitHubIcon className={classes.icon} color="primary" style={{ height: "4%", width: "4%", }} /></a>
@@ -184,8 +270,20 @@ export default function Footer() {
               < TwitterIcon className={classes.icon} color="primary" style={{ height: "4%", width: "4%" }} /> </a>
             <a href="https://www.linkedin.com/in/ashish2495/" target="_blank">
               < LinkedInIcon className={classes.icon} color="primary" style={{ height: "4%", width: "4%" }} /> </a>
-
           </div>
+           )}
+           {width <= 720 && (
+          <div style={{ margin: "1% 1% 0% 1%" }}>
+            <a href="https://github.com/ASHISH-GITHUB2495/GRIP_BasicBankingSystem_2021" target="_blank">
+              < GitHubIcon className={classes.icon} color="primary" style={{ height: "8%", width: "8%", }} /></a>
+            <a href="https://ashish-github2495.github.io/MyPortfolio.github.io/" target="_blank">
+              < FacebookIcon className={classes.icon} color="primary" style={{ height: "8%", width: "8%" }} /></a>
+            <a href="https://www.stopstalk.com/user/profile/ZeroHu0" target="_blank">
+              < TwitterIcon className={classes.icon} color="primary" style={{ height: "8%", width: "8%" }} /> </a>
+            <a href="https://www.linkedin.com/in/ashish2495/" target="_blank">
+              < LinkedInIcon className={classes.icon} color="primary" style={{ height: "8%", width: "8%", marginLeft:"-5px" }} /> </a>
+          </div>
+           )}
         </FormControl>
 
 
