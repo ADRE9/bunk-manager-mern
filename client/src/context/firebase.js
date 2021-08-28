c5873cd38b5d98062b6a203d9f6dd80979655aa5
Adrenine <adrenine13@gmail.com>
2021-08-28 16:11:12 +0530

    firebase config file for autentication under dev

---
+++ client/src/context/firebase.js
@@ -0,0 +1,17 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD8aHu-kYXj-syRoMqnvCIdqqG8o1l_Hu4",
+//   authDomain: "bunkmanager-796ad.firebaseapp.com",
+//   projectId: "bunkmanager-796ad",
+//   storageBucket: "bunkmanager-796ad.appspot.com",
+//   messagingSenderId: "445028324031",
+//   appId: "1:445028324031:web:ee385eac8972dfc825ac0a"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
